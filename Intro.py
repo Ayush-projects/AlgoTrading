cb8595033a77adc9dd3e73bed95361f50e48e9b0
Ayush <kayush479@gmail.com>
2022-11-27 18:00:33 +0530

    Ayush

---
+++ Intro.py
@@ -0,0 +1,10 @@
+#Algorithmic trading means using computers to make investment decisions
+#There are many different types of algorithmic trading. The main difference is their speed of execution.
+
+
+#The Algorithmic Trading Process
+
+#The process of running a quantitative investing strategy can be broken down into the following steps:
+# 1. Collect Data
+# 2. Develop a hypothesis for a strategy
+# 3. Backtest that strategy 
