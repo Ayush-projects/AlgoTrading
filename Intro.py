c5728c406ed8570fe809a645ee0bc12b1382ba4b
Ayush <kayush479@gmail.com>
2022-11-27 18:27:32 +0530

    Add proj desc

--- Intro.py
+++ Intro.py
@@ -2,9 +2,15 @@
 #There are many different types of algorithmic trading. The main difference is their speed of execution.
 
 
-#The Algorithmic Trading Process
+#The Algorithmic Trading Process:-
 
 #The process of running a quantitative investing strategy can be broken down into the following steps:
 # 1. Collect Data
 # 2. Develop a hypothesis for a strategy
 # 3. Backtest that strategy 
+
+#In this repo all the strategies will be using random data than that we are going to use in production.
+
+
+#API - Application Programming Interface, In this repo we'll using IEX cloud API to gather stock market data to make investment decision
+
